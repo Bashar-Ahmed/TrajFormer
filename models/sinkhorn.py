c6fbb08e2e2bccf6d0f2ca017da5d336579c1022
Bashar-Ahmed <basharahmed00000@gmail.com>
2023-03-05 09:03:10 +0000

    handle div by 0 error

--- models/sinkhorn.py
+++ models/sinkhorn.py
@@ -1,11 +1,13 @@
 import torch
 
-def sinkhorn(x, max_iter=3, epsilon=1e-7):
+def sinkhorn(x, max_iter=2, epsilon=1e-8):
     
-    k = torch.softmax(x, dim=-1)
+    factor = x.shape[-2]/x.shape[-1]
+    k = x.softmax(dim=-1)
     for i in range(max_iter):
-        col_sum = torch.sum(k, -2, keepdim=True) + epsilon
-        k = k / col_sum
-        row_sum = torch.sum(k, -1, keepdim=True) + epsilon
-        k = k / row_sum
+        col_sum = k.sum(dim=-2, keepdim=True)
+        col_sum[col_sum<=epsilon] = 1.
+        k = k.div(col_sum)
+        row_sum = k.sum(dim=-1, keepdim=True)
+        k = k.div(row_sum)
     return k
